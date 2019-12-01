e652b6d04862c6ba1c7753462e1a142f722c6dfc
kim tae jun <xowns4817@naver.com>
2019-12-01 15:36:14 +0900

    2019-12-01 저장소

---
+++ src/main/java/com/project/manlihyang/ManlihyangApplication.java
@@ -0,0 +1,13 @@
+package com.project.manlihyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManlihyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManlihyangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/manlihyang/ManlihyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.manlihyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManlihyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
