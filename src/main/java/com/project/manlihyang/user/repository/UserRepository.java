75c8fa1e7a7630e4c55bdb93e4617b73466362f5
phantasmicmeans <tkdals!12>
2019-12-20 22:52:33 +0900

    [USER] EMAIL 검증 API 반영

--- src/main/java/com/project/manlihyang/user/repository/UserRepository.java
+++ src/main/java/com/project/manlihyang/user/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.project.manlihyang.user.repository;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+@Mapper
 @Repository
 public interface UserRepository {
+
+    @Select("SELECT EXISTS (SELECT email FROM user WHERE email=#{email})")
+    boolean isExistsEmail(@Param("email") String email);
 
 }
