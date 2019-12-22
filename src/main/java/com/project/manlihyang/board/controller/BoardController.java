57b615725470f6b136b5e7817fb6398d0538cc6f
phantasmicmeans <tkdals!12>
2019-12-23 00:32:39 +0900

    [USER] USER 생성, 조회, Email 중복체크 기능 추가

--- src/main/java/com/project/manlihyang/board/controller/BoardController.java
+++ src/main/java/com/project/manlihyang/board/controller/BoardController.java
@@ -1,10 +1,11 @@
 package com.project.manlihyang.board.controller;
 
+import com.project.manlihyang.board.domain.Board;
 import com.project.manlihyang.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/Board")
@@ -13,8 +14,37 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping("/list")
-    public String board_list( ) {
+    //게시물 조회 ( 전체 )
+    @GetMapping("")
+    public ArrayList<Board> board_read( ) {
         return boardService.ReadBoards();
     }
+
+    //특정 게시물 상세조회
+    @GetMapping("/{id}")
+    public void board_read_detail(@PathVariable("id") String board_id){
+
+    }
+
+    //게시물 삽입
+    @PostMapping("")
+    public int board_insert(Board board) {
+        return boardService.CreateBoard(board);
+    }
+
+    //게시물 수정
+
+    //게시물 삭제
+
+    //게시물 좋아요
+
+    //게시물 신고
+
+    //댓글 조회
+
+    //댓글 삽입
+
+    //댓글 삭제
+
+    //댓글 수정
 }
