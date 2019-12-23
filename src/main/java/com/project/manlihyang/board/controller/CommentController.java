4e2d61dc1be37fedba0a45228f23ebc0d1e0a22b
kim tae jun <xowns4817@naver.com>
2019-12-24 00:21:53 +0900

    20191224 test

---
+++ src/main/java/com/project/manlihyang/board/controller/CommentController.java
@@ -0,0 +1,26 @@
+package com.project.manlihyang.board.controller;
+
+
+import com.project.manlihyang.util.Const;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board/comment")
+@CrossOrigin(origins = "*")
+public class CommentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+
+    //댓글 달기
+
+    //댓글 수정
+
+    //댓글 삭제
+
+
+}
